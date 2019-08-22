1791b7b71bf1134992af0659c4a973349aed870e
Michael Grinf eld <michael.grinfeld@dynamicyield.com>
2019-08-22 10:51:04 +0300

    added first simple test

--- src/test/java/com/mikerusoft/redirect/to/stream/services/RedirectPublisherTest.java
+++ src/test/java/com/mikerusoft/redirect/to/stream/services/RedirectPublisherTest.java
@@ -6,11 +6,14 @@ import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableOnSubscribe;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import javax.inject.Inject;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @MicronautTest
 class RedirectPublisherTest {
@@ -19,15 +22,17 @@ class RedirectPublisherTest {
     private RedirectService<RequestWrapper, FlowableOnSubscribe<RequestWrapper>> service;
 
     @Test
-    void test() throws Exception {
+    @Timeout(value = 300L, unit = TimeUnit.MILLISECONDS)
+    void when1EventEmitted_expected1Event() throws Exception {
         Flowable<RequestWrapper> retrieve = Flowable.create(service.subscriber(), BackpressureStrategy.BUFFER);
         Executors.newSingleThreadScheduledExecutor().schedule(() -> {
-            service.emit(RequestWrapper.builder().uri("/bla/bla").build());
-            service.emit(RequestWrapper.builder().uri("/b/b").build());
+            service.emit(RequestWrapper.builder().method("GET").uri("/bla/bla").build());
         }, 100, TimeUnit.MILLISECONDS);
         RequestWrapper requestWrapper = retrieve.blockingFirst();
-        System.out.println(requestWrapper);
-        Thread.sleep(10000L);
+        assertThat(requestWrapper).isNotNull()
+            .hasFieldOrPropertyWithValue("method", "GET")
+            .hasFieldOrPropertyWithValue("uri", "/bla/bla")
+        ;
     }
 
 }
