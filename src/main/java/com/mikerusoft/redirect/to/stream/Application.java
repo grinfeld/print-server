e929b5b564cc03a6c00e2f99ecbcde45a6176190
Mikhail Grinfeld <grinfeld@gmail.com>
2019-09-24 08:53:22 +0300

    added the main class

---
+++ src/main/java/com/mikerusoft/redirect/to/stream/Application.java
@@ -0,0 +1,10 @@
+package com.mikerusoft.redirect.to.stream;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
