3c954c2a6e55936edcc8e6682f34daef33fc1847
Michael Grinf eld <michael.grinfeld@dynamicyield.com>
2019-11-27 17:03:35 +0200

    remove unused import

--- src/main/java/com/mikerusoft/redirect/to/stream/services/RedirectService.java
+++ src/main/java/com/mikerusoft/redirect/to/stream/services/RedirectService.java
@@ -1,7 +1,5 @@
 package com.mikerusoft.redirect.to.stream.services;
 
-import io.reactivex.functions.Action;
-
 public interface RedirectService<T, F> {
     void emit(T element);
     F subscriber();
