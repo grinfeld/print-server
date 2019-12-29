c6e168021c1e742da3ab78e797d70267d0aefb1f
Michael Grinf eld <michael.grinfeld@dynamicyield.com>
2019-12-29 18:12:12 +0200

    added delayed end-point

--- src/main/java/com/mikerusoft/redirect/to/stream/receiver/http/ReceiveStatusController.java
+++ src/main/java/com/mikerusoft/redirect/to/stream/receiver/http/ReceiveStatusController.java
@@ -1,5 +1,6 @@
 package com.mikerusoft.redirect.to.stream.receiver.http;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.mikerusoft.redirect.to.stream.model.BasicRequestWrapper;
 import com.mikerusoft.redirect.to.stream.services.RedirectService;
 import com.mikerusoft.redirect.to.stream.services.UrlCalculation;
@@ -9,12 +10,15 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.Flowable;
 import io.reactivex.FlowableOnSubscribe;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static com.mikerusoft.redirect.to.stream.receiver.http.HttpUtils.extractRequest;
 
@@ -24,7 +28,7 @@ public class ReceiveStatusController {
 
     private UrlCalculation responser;
     private RedirectService<BasicRequestWrapper, FlowableOnSubscribe<BasicRequestWrapper>> service;
-
+    private RateLimiter rateLimiter;
     private GlobalParams params;
 
     public ReceiveStatusController(RedirectService<BasicRequestWrapper, FlowableOnSubscribe<BasicRequestWrapper>> service, UrlReceiverProperties props) {
@@ -32,37 +36,70 @@ public class ReceiveStatusController {
         if (props.getInactiveUrlExpireSec() <= 0)
             throw new IllegalArgumentException("Inactive urls expiration should be greater than 0");
         this.responser = new UrlCalculationByStateCache(props.getInactiveUrlExpireSec());
-        this.params = GlobalParams.builder().globalStatus(props.getGlobalStatus()).globalFreq(props.getGlobalFrequencyCount()).build();
+        this.rateLimiter = RateLimiter.create(props.getGlobalDelayMs());
+        this.params = GlobalParams.builder().globalStatus(props.getGlobalStatus())
+                .globalDelay(props.getGlobalDelayMs())
+                .globalFreq(props.getGlobalFrequencyCount()).build();
     }
 
     @Get("/status/{status}/freq/{freq}/{/get:.*}")
-    public HttpResponse statusOnly(HttpRequest<?> request, final int status, int freq, Optional<String> get) {
-        return statusOnly(request, status, freq, get, null);
+    public HttpResponse<?> statusOnly(HttpRequest<?> request, final int status, int freq, Optional<String> get) {
+        return generateStatusResponse(request, status, freq, null);
     }
 
     @Post("/status/{status}/freq/{freq}/{/post:.*}")
-    public HttpResponse statusOnly(HttpRequest<?> request, int status, int freq, Optional<String> post, @Body String body) {
+    public HttpResponse<?> statusOnly(HttpRequest<?> request, int status, int freq, Optional<String> post, @Body String body) {
+        return generateStatusResponse(request, status, freq, body);
+    }
+
+    @Post("/status/global/{/post:.*}")
+    public HttpResponse<?> globalStatusOnly(HttpRequest<?> request, Optional<String> post, @Body String body) {
+        return generateStatusResponse(request, params.getGlobalStatus(), params.getGlobalFreq(), body);
+    }
+
+    @Get("/status/global/{/get:.*}")
+    public HttpResponse<?> globalStatusOnly(HttpRequest<?> request, Optional<String> get) {
+        return generateStatusResponse(request, params.getGlobalStatus(), params.getGlobalFreq(), null);
+    }
+
+    @Get("/global/change/status/{status}/freq/{freq}")
+    public HttpResponse<?> changeGlobal(@PathVariable("status") int status, @PathVariable("freq") int freq) {
+        this.params = this.params.toBuilder().globalFreq(freq).globalStatus(status).build();
+        return HttpResponse.ok();
+    }
+
+    @Get("/global/change/delay/{delay}")
+    public HttpResponse<?> changeGlobalDelay(@PathVariable("delay") long delay) {
+        this.params = this.params.toBuilder().globalDelay(delay).build();
+        return HttpResponse.ok();
+    }
+
+    public HttpResponse<?> generateStatusResponse(HttpRequest<?> request, int status, int freq, String body) {
         String uri = request.getUri().toString();
         service.emit(extractRequest(request, body));
         return responser.getValue(uri, freq, HttpResponse::ok, () -> HttpResponse.status(HttpStatus.valueOf(status)));
     }
 
-    @Post("/status/global/{/post:.*}")
-    public HttpResponse globalStatusOnly(HttpRequest<?> request, Optional<String> post, @Body String body) {
+    @Get("/delay/global/{/get:.*}")
+    public HttpResponse<?> globalGetWithDelay(HttpRequest<?> request, Optional<String> get) {
+        return generateStatusResponseWithDelay(request, HttpResponseStatus.OK.code(), params.getGlobalFreq(), null);
+    }
+
+    @Post("/delay/global/{/post:.*}")
+    public HttpResponse<?> globalPostWithDelay(HttpRequest<?> request, Optional<String> post, @Body String body) {
+        return generateStatusResponseWithDelay(request, HttpResponseStatus.OK.code(), params.getGlobalFreq(), body);
+    }
+
+    public HttpResponse<?> generateStatusResponseWithDelay(HttpRequest<?> request, int status, int freq, String body) {
         String uri = request.getUri().toString();
         service.emit(extractRequest(request, body));
-        return responser.getValue(uri, params.getGlobalFreq(), HttpResponse::ok, () -> HttpResponse.status(HttpStatus.valueOf(params.getGlobalStatus())));
+        return responser.getValue(uri, freq, HttpResponse::ok, () -> delayed(HttpResponse.status(HttpStatus.valueOf(status))));
     }
 
-    @Get("/status/global/{/get:.*}")
-    public HttpResponse globalStatusOnly(HttpRequest<?> request, Optional<String> get) {
-        return globalStatusOnly(request, get, null);
-    }
-
-    @Get("/global/change/status/{status}/freq/{freq}")
-    public HttpResponse changeGlobal(@PathVariable("status") int status, @PathVariable("freq") int freq) {
-        this.params = this.params.toBuilder().globalFreq(freq).globalStatus(status).build();
-        return HttpResponse.ok();
+    private HttpResponse<?> delayed(HttpResponse<?> status) {
+        boolean success = rateLimiter.tryAcquire();
+        return success ? Flowable.just(status)
+                .delay(params.getGlobalDelay(), TimeUnit.MILLISECONDS).blockingFirst() : status;
     }
 
     @Data
@@ -71,6 +108,7 @@ public class ReceiveStatusController {
     private static class GlobalParams {
         private int globalStatus;
         private int globalFreq;
+        private long globalDelay;
     }
 
     // for tests only
--- src/main/java/com/mikerusoft/redirect/to/stream/utils/UrlReceiverProperties.java
+++ src/main/java/com/mikerusoft/redirect/to/stream/utils/UrlReceiverProperties.java
@@ -8,28 +8,17 @@ public class UrlReceiverProperties {
     private int inactiveUrlExpireSec;
     private int globalFrequencyCount;
     private int globalStatus;
+    private long globalDelayMs;
+    private int maxSizeOfDelayedRequests;
 
-    public int getInactiveUrlExpireSec() {
-        return inactiveUrlExpireSec;
-    }
-
-    public void setInactiveUrlExpireSec(int inactiveUrlExpireSec) {
-        this.inactiveUrlExpireSec = inactiveUrlExpireSec;
-    }
-
-    public int getGlobalFrequencyCount() {
-        return globalFrequencyCount;
-    }
-
-    public void setGlobalFrequencyCount(int globalFrequencyCount) {
-        this.globalFrequencyCount = globalFrequencyCount;
-    }
-
-    public int getGlobalStatus() {
-        return globalStatus;
-    }
-
-    public void setGlobalStatus(int globalStatus) {
-        this.globalStatus = globalStatus;
-    }
+    public int getInactiveUrlExpireSec() { return inactiveUrlExpireSec;}
+    public void setInactiveUrlExpireSec(int inactiveUrlExpireSec) { this.inactiveUrlExpireSec = inactiveUrlExpireSec; }
+    public int getGlobalFrequencyCount() { return globalFrequencyCount; }
+    public void setGlobalFrequencyCount(int globalFrequencyCount) { this.globalFrequencyCount = globalFrequencyCount; }
+    public int getGlobalStatus() { return globalStatus; }
+    public void setGlobalStatus(int globalStatus) { this.globalStatus = globalStatus; }
+    public long getGlobalDelayMs() { return globalDelayMs; }
+    public void setGlobalDelayMs(long globalDelayMs) { this.globalDelayMs = globalDelayMs; }
+    public int getMaxSizeOfDelayedRequests() { return maxSizeOfDelayedRequests; }
+    public void setMaxSizeOfDelayedRequests(int maxSizeOfDelayedRequests) { this.maxSizeOfDelayedRequests = maxSizeOfDelayedRequests; }
 }
